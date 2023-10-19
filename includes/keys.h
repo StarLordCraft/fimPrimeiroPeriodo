0b302685cb97d017c2ee6fca01bb9cd4736e1f05
Rafael <rafa.viana.moc@gmail.com>
2023-10-19 14:41:39 -0300

    crossplatform defining keys

--- includes/keys.h
+++ includes/keys.h
@@ -1,10 +1,26 @@
 #ifndef KEYS_H
 #define KEYS_H
 
-#define KEY_DOWN	0402		/* down-arrow key */
-#define KEY_UP		0403		/* up-arrow key */
-#define KEY_LEFT	0404		/* left-arrow key */
-#define KEY_RIGHT	0405		/* right-arrow key */
-#define KEY_BACKSPACE	0407
+#ifdef _WIN32
+
+#define KEY_DOWN        40      /* down-arrow key */
+#define KEY_UP          38      /* up-arrow key */
+#define KEY_LEFT        37      /* left-arrow key */
+#define KEY_RIGHT       39      /* right-arrow key */
+#define KEY_BACKSPACE   8
+#define KEY_ENTER       13      /* tecla Enter */
+#define KEY_SPACE       32      /* tecla Espaço */
+
+#else
+
+#define KEY_DOWN        0402    /* down-arrow key */
+#define KEY_UP          0403    /* up-arrow key */
+#define KEY_LEFT        0404    /* left-arrow key */
+#define KEY_RIGHT       0405    /* right-arrow key */
+#define KEY_BACKSPACE   0407
+#define KEY_ENTER       012     /* tecla Enter */
+#define KEY_SPACE       040     /* tecla Espaço */
+
+#endif
 
 #endif
